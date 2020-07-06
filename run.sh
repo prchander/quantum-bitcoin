be8d1d47f32fdd6d7542f397ea47317f50c680d9
SimeoW <swuthier@uccs.edu>
2020-07-06 14:08:35 -0600

    Working regtest

--- run.sh
+++ run.sh
@@ -14,11 +14,10 @@ echo "datadir = $dir"
 
 if [ ! -f "$dir/bitcoin.conf" ]; then
 	echo "Resetting configuration file"
-	echo "regtest=1" > "$dir/bitcoin.conf"
 	echo "server=1" > "$dir/bitcoin.conf"
 	echo "rpcuser=cybersec" >> "$dir/bitcoin.conf"
 	echo "rpcpassword=kZIdeN4HjZ3fp9Lge4iezt0eJrbjSi8kuSuOHeUkEUbQVdf09JZXAAGwF3R5R2qQkPgoLloW91yTFuufo7CYxM2VPT7A5lYeTrodcLWWzMMwIrOKu7ZNiwkrKOQ95KGW8kIuL1slRVFXoFpGsXXTIA55V3iUYLckn8rj8MZHBpmdGQjLxakotkj83ZlSRx1aOJ4BFxdvDNz0WHk1i2OPgXL4nsd56Ph991eKNbXVJHtzqCXUbtDELVf4shFJXame" >> "$dir/bitcoin.conf"
-	echo "rpcport=8332" >> "$dir/bitcoin.conf"
+	#echo "rpcport=8332" >> "$dir/bitcoin.conf"
 	#echo "rpcallowip=0.0.0.0/0" >> "$dir/bitcoin.conf"
 	#echo "rpcbind = 0.0.0.0:8332" >> "$dir/bitcoin.conf"
 	#echo "upnp=1" >> "$dir/bitcoin.conf"
@@ -29,10 +28,10 @@ if [ "$1" == "gui" ]; then
 	if [ "$pruned" == "true" ]; then
 		echo "Pruned mode activated, only keeping 550 block transactions"
 		echo
-		src/qt/bitcoin-qt -prune=550 -datadir="$dir" -debug=researcher
+		src/qt/bitcoin-qt -regtest -prune=550 -datadir="$dir"
 	else
 		echo
-		src/qt/bitcoin-qt -datadir="$dir" -debug=researcher
+		src/qt/bitcoin-qt -regtest -datadir="$dir"
 	fi
 else
 	
@@ -51,9 +50,8 @@ else
 	if [ "$pruned" == "true" ]; then
 		echo "Pruned mode activated, only keeping 550 block transactions"
 		echo
-		src/bitcoind -prune=550 -datadir="$dir" -debug=researcher
+		src/bitcoind -regtest -prune=550 -datadir="$dir"
 	else
 		echo
-		src/bitcoind -datadir="$dir" -debug=researcher
-	fi
+		src/bitcoind -regtest -datadir="$dir"
 fi
